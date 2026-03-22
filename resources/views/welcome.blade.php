b3ac33338190d785eed477c2328417e6bb7e1e62
muhasfi <muhammadhasfi15@gmail.com>
2026-03-22 12:25:45 +0700

    icon fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="{{ asset('css/idonation.css') }}">
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('idonation.svg') }}">
 </head>
 <body>
 
